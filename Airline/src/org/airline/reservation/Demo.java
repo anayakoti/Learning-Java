55d158763c0fd7248d57e4315f9dc7ae66506218
anayakoti <anayakoti@hotmail.com>
2019-10-04 09:22:43 -0500

    Create a new package

---
+++ Airline/src/org/airline/reservation/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.airline.reservation;
+
+/**
+ * @author nayak
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First program in Eclipse IDE");
+
+	}
+
+}
