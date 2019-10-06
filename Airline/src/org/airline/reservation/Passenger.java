ef8471483fbcdaf298863eef8cd3ee1dbca2cfce
anayakoti <anayakoti@hotmail.com>
2019-10-05 19:44:10 -0500

    Added multiline comments and inline-comments

--- Airline/src/org/airline/reservation/Passenger.java
+++ Airline/src/org/airline/reservation/Passenger.java
@@ -1,5 +1,20 @@
+/* Passenger.java
+ * Written by Anudeep Nayakoti
+ * We have created a contructor, and also declared field as name for Passengers
+ * 
+ */
 package org.airline.reservation;
 
 public class Passenger {
+	
+ //Creating the fields
+	private String name;
+	
+	//Lets create a constructor
+	public Passenger() {
+		name="Not known Yet!";
+		//Names are given while create new object or new passenger to give different name.
+		System.out.println(name);
+	}
 
 }
