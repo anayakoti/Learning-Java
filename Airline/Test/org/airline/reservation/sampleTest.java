2eb223606707fe4285e615f40d2c3bcd0f881890
anayakoti <anayakoti@hotmail.com>
2019-10-08 08:03:54 -0500

    New test file has been created by the Juniper Java testing framework

---
+++ Airline/Test/org/airline/reservation/sampleTest.java
@@ -0,0 +1,24 @@
+package org.airline.reservation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sampleTest {
+
+	@Test
+	void testPassenger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetterName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+}
