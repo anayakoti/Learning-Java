fbee9404f2f59bb7b40ee5914b49ef5b5ce705f5
anayakoti <anayakoti@hotmail.com>
2019-10-10 10:53:20 -0500

    Created a failed test purposefully like in the test driven environment

--- Airline/test/org/airline/reservation/FlightTest.java
+++ Airline/test/org/airline/reservation/FlightTest.java
@@ -8,7 +8,12 @@ class FlightTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+	//Lets create flight objects
+		Flight testFlight1 = new Flight(100);
+		assertEquals("Unknowncity", testFlight1.getDepartureCity());
+		assertEquals("UnknownCountry", testFlight1.getArrivalCity());
+		assertEquals(100,testFlight1.getFlightNumber);
+		
 	}
 
 }
