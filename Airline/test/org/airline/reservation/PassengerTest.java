baa0fb63a241ffc6d8d5216baa4569dc312e97c7
anayakoti <anayakoti@hotmail.com>
2019-10-10 07:38:07 -0500

    Made a sample Jnuit test successfully

---
+++ Airline/test/org/airline/reservation/PassengerTest.java
@@ -0,0 +1,26 @@
+package org.airline.reservation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PassengerTest {
+
+	@Test
+	public void testPassenger() {
+	 Passenger testPassenger1=new Passenger();
+	 String result=testPassenger1.getterName();
+	 assertEquals("Not known Yet!", result);
+	}
+
+	@Test
+	public void testGetterName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+}
