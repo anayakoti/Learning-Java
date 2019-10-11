e008444d03119601953476a2e9d5b6002a85b572
anayakoti <anayakoti@hotmail.com>
2019-10-11 07:13:05 -0500

    Created TestSeatnumber in a TTD format and created Seat class without code assistant

---
+++ Airline/src/org/airline/reservation/Seat.java
@@ -0,0 +1,21 @@
+package org.airline.reservation;
+
+public class Seat {
+ public int seatNumber;
+ 
+ 
+//Lets create a constructor
+ public Seat() {
+	 seatNumber=1202;
+ }
+ 
+ public  int getSeatNumber()
+ {
+	return seatNumber; 
+ }
+ public void setSeatNumber(int seatNumber) {
+	this.seatNumber=seatNumber;
+	 
+ }
+ 
+}
---
+++ Airline/test/org/airline/reservation/seatTest.java
@@ -0,0 +1,26 @@
+package org.airline.reservation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class seatTest {
+
+	@Test
+	public void testSeat() {
+	//Now lets test the constructor 
+		Seat seatNumber1 = new Seat();
+		assertEquals(1202, seatNumber1.getSeatNumber());
+		
+	}
+	
+	@Test
+	public void testSetter() {
+	//Now lets test the setters
+		Seat seatNumber2 = new Seat();
+		seatNumber2.setSeatNumber(12);
+	    assertEquals(12, seatNumber2.getSeatNumber());
+		
+	}
+
+}
