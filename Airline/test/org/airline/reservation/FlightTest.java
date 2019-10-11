88bc406776d4c76aca4b96298a7259083cc1fc7b
anayakoti <anayakoti@hotmail.com>
2019-10-11 06:08:51 -0500

    Created test setters and assigned new values by creating new objects to the Flight class

--- Airline/test/org/airline/reservation/FlightTest.java
+++ Airline/test/org/airline/reservation/FlightTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class FlightTest {
 
 	@Test
-	void test() {
+	public void flightTest() {
 	//Lets create flight objects
 		Flight testFlight1 = new Flight();
 		assertEquals("unknownCity", testFlight1.getDepartureCity());
@@ -15,5 +15,36 @@ class FlightTest {
 		assertEquals(100,testFlight1.getFlightNumber());
 		
 	}
+	//Testing the setter
+	@Test
+	public void departureCityTest() 
+	{
+	//While testing the setter methods, we need to create a new objects
+		Flight flightTest1 = new Flight();
+		flightTest1.setDepartureCity("Hyderabad");
+		assertEquals("Hyderabad", flightTest1.getDepartureCity());
+		
+		
+	}
+	@Test
+	public void ArrivalCityTest() 
+	{
+	//While testing the setter methods, we need to create a new objects
+		Flight flightTest2 = new Flight();
+		flightTest2.setArrivalCity("Detriot");
+		assertEquals("Detriot", flightTest2.getArrivalCity());
+		
+		
+	}
 
+	@Test
+	public void FlightNumberTest() 
+	{
+	//While testing the setter methods, we need to create a new objects
+		Flight flightTest3 = new Flight();
+		flightTest3.setFlightNumber(1202);
+		assertEquals(100, flightTest3.getFlightNumber());
+		
+		
+	}
 }
