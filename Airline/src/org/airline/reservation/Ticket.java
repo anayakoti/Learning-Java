774ea6cde472eb3870b0a00abb1835e86b931f2c
anayakoti <anayakoti@hotmail.com>
2019-10-13 09:57:52 -0500

    Created a new class ticket and added Fields of passanger of type 'Passenger' and flight of type 'Flight' and seat of type 'Seat'

---
+++ Airline/src/org/airline/reservation/Ticket.java
@@ -0,0 +1,60 @@
+package org.airline.reservation;
+
+import java.time.LocalDate;
+
+public class Ticket {
+private LocalDate departureDate;
+private Passenger passenger;
+private Flight flight;
+private Seat seat;
+
+
+//Lets create a constructor
+public Ticket() {
+departureDate=LocalDate.now();//This will grab the system date of the local machine and set it to the variable//
+	
+}
+
+
+public LocalDate getDepartureDate() {
+	return departureDate;
+}
+
+
+public void setDepartureDate(LocalDate departureDate) {
+	this.departureDate = departureDate;
+}
+
+
+public Passenger getPassenger() {
+	return passenger;
+}
+
+
+public void setPassenger(Passenger passenger) {
+	this.passenger = passenger;
+}
+
+
+public Flight getFlight() {
+	return flight;
+}
+
+
+public void setFlight(Flight flight) {
+	this.flight = flight;
+}
+
+
+public Seat getSeat() {
+	return seat;
+}
+
+
+public void setSeat(Seat seat) {
+	this.seat = seat;
+}
+
+
+
+}
