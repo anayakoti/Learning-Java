bb18e80a97c4e55a747e1ce88f41cac736b05ab7
anayakoti <anayakoti@hotmail.com>
2019-10-21 19:36:13 -0500

    Made changes

--- SecondModuleOfEdx/src/second/logic/BooleanTest.java
+++ SecondModuleOfEdx/src/second/logic/BooleanTest.java
@@ -11,19 +11,21 @@ public class BooleanTest {
 	int y=3;
 	int z=4;
 	
-	//Checking if Z is odd 
+	//Checking if Z is not greater than squareroot of y 
+	double squarerootOfY=Math.sqrt(y);
+ 
 	
-	
-	if(z%3==0) {
-		System.out.println("Z is an Odd number");
-		
+	if(z>squarerootOfY) {
+		System.out.println("The value of Z is greater than the squareRoot of Y");
 	}
 	else {
-		System.out.println("Z is not an Odd number");
+		System.out.println("It is not Greater");
 	}
 	
+	  
+
 	}
-	}
+}
 	
 	
 	
