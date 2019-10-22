6b79ac90ba32960a41be01320a239e10ecb30eee
anayakoti <anayakoti@hotmail.com>
2019-10-22 12:13:36 -0500

    Made pyramid kind of Numbers

---
+++ SecondModuleOfEdx/src/second/logic/NumberTriangle.java
@@ -0,0 +1,20 @@
+package second.logic;
+
+public class NumberTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int row=1; row<=7;row++)
+     {
+    	 
+    	 for (int column=1; column<=row;column++)
+    	 {
+    		 System.out.print(row+"");
+    		 
+    	 }
+    	 System.out.println("");
+    	 }
+     
+     }
+	}
+
