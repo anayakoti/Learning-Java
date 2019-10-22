178976e672f2b7b91a08a37a1936496cd4bb222b
anayakoti <anayakoti@hotmail.com>
2019-10-22 11:44:51 -0500

    Made changes:

--- SecondModuleOfEdx/src/second/logic/LoopFor.java
+++ SecondModuleOfEdx/src/second/logic/LoopFor.java
@@ -21,7 +21,11 @@ public static void main(String args[])
 		System.out.println(+tableNumber+"*"+i+" = "+(tableNumber*i));
 
 	}
-
+		for (int i = 0; i < 10; i++) {
+		    for (int j = 0; j < 5; j++) {
+		        System.out.print("*");
+		    }
+		}
 	 
 }
 
---
+++ SecondModuleOfEdx/src/second/logic/NestedForLoop.java
@@ -0,0 +1,29 @@
+package second.logic;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		for(int rows=1; rows<=5;rows++) {
+			
+			System.out.print("Row:"+rows+"-->");
+			
+			
+			for(int column=0; column<10; column++) {
+				
+			System.out.print("Cnumber:"+column+" |");
+			
+				
+				
+				
+			}
+			System.out.println("");
+			
+		}
+		
+		
+		
+	}
+
+}
