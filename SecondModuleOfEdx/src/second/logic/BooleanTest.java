2bca64719190f3f8cc827ba0cafdfba87998828d
anayakoti <anayakoti@hotmail.com>
2019-10-21 19:31:28 -0500

    Started second module

---
+++ SecondModuleOfEdx/src/second/logic/BooleanTest.java
@@ -0,0 +1,30 @@
+package second.logic;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+	int x=1;
+	int y=3;
+	int z=4;
+	
+	//Checking if Z is odd 
+	
+	
+	if(z%3==0) {
+		System.out.println("Z is an Odd number");
+		
+	}
+	else {
+		System.out.println("Z is not an Odd number");
+	}
+	
+	}
+	}
+	
+	
+	
+
