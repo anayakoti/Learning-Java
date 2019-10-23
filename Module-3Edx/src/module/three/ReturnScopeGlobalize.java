3bb22e216c4f8d1299d03f430c50fe1e0c6adb4c
anayakoti <anayakoti@hotmail.com>
2019-10-22 21:32:54 -0500

    Used Return statement inside a parameter

--- Module-3Edx/src/module/three/ReturnScopeGlobalize.java
+++ Module-3Edx/src/module/three/ReturnScopeGlobalize.java
@@ -6,6 +6,11 @@ public class ReturnScopeGlobalize {
 		// TODO Auto-generated method stub
      int x=5;
      method1(x);
+     System.out.println("The Value of X inside the main is still :"+x);
+     int y=6;
+     System.out.println("Before return value of Y is :"+y);
+     y=method2(y);
+     System.out.println("The value of computed y returned from the method to main is now:  "+y);
 	}
 	
 	
@@ -16,6 +21,13 @@ public class ReturnScopeGlobalize {
 		x=x*5;
 		
 		System.out.println("The computed value of X inside a method is:"+x);
+	}
+	
+	//Lets use the return to print the information of a variable within the method to a Main
+	
+	public static int method2(int y) {
+		y=y*4;
+		return y;
 	}
 	
 	
