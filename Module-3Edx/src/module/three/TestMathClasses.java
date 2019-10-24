0cdc40579bf10af86d8f436f907da24cb3049e3b
anayakoti <anayakoti@hotmail.com>
2019-10-23 19:33:30 -0500

    Worked with different math functions

---
+++ Module-3Edx/src/module/three/TestMathClasses.java
@@ -0,0 +1,27 @@
+package module.three;
+
+public class TestMathClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println(Math.pow(5/2, 6));
+		System.out.println(Math.ceil(9.1));//Prints one number above
+		System.out.println(Math.ceil(115.9));
+		System.out.println(Math.min(-2,-5));
+		System.out.println(Math.sqrt(25));
+		System.out.println(100+Math.log10(100));
+		System.out.println(Math.max(18 - 5 + Math.random(), Math.ceil(4.6 * 3)));
+		System.out.println(7 - 2 + Math.log10(1000) + Math.log(Math.pow(Math.E, 5)));
+		System.out.println(Math.round(2.7));
+		System.out.println(Math.floor(2.9));
+		//in order to generate a Random number between 1 to 10 the best way is 
+		System.out.println(Math.random()*10);
+		
+		
+		
+	
+	}
+
+}
