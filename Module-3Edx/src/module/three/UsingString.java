28aa72bfe6dd63336eaeb5575254c4fa67d4ab7e
anayakoti <anayakoti@hotmail.com>
2019-10-24 07:41:18 -0500

    Wrote a program to print all the characters in a String using CharAT

---
+++ Module-3Edx/src/module/three/UsingString.java
@@ -0,0 +1,16 @@
+package module.three;
+
+public class UsingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//we can use String methods to print all the characters.
+		
+		String name="anudeep";
+		for(int i=0; i<name.length();i++)
+		{
+			System.out.println("The Index of "+i +"is:" +name.charAt(i));
+		}
+	}
+
+}
